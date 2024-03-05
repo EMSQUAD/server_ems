e65ffc5d256fc8722f9a41532808d30f7fc8f514
noy2611 <noyr2611@gmail.com>
2024-03-05 15:39:16 +0200

    06

--- router/user.router.js
+++ router/user.router.js
@@ -20,6 +20,8 @@ userRouter.get('/', userController.getAllUsers);
  userRouter.post('/login', userController.loginUser);
 
 
- userRouter.post('/updateExpoPushToken', userController.updateExpoPushToken);
+//  userRouter.post('/updateExpoPushToken', userController.updateExpoPushToken);
 
+
+userRouter.post('/updateMessage', userController.updateExpoPushToken);
 module.exports = { userRouter };
